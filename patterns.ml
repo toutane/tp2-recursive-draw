29d446ce12bc8bf72521cd17c73eb0ad991c261e
Charles-Antoine Leger <ca@leger.email>
2021-10-05 11:40:57 +0200

    Add Bonus section functions.

--- patterns.ml
+++ patterns.ml
@@ -1,8 +1,10 @@
 (* 1.1.1. build_line function returns a string of n times the string str *)
 
 let rec build_line n str =
-  if n = 1 then str
-  else str ^ build_line (n - 1) str
+  match n with
+    0 -> ""
+  | 1 -> str
+  | _ -> str ^ build_line (n - 1) str
 ;;
 
 build_line 5 "*" ;;
@@ -60,10 +62,10 @@ let triangle n str =
   let x = n
   in let rec print_line n =
        let a = x - (n - 1)
-       in if n = 1 then
-            (
-              print_string (build_line a str) ;print_newline()
-            )
+       in if n = 1 then (
+            print_string (build_line a str) ;
+            print_newline()
+          )
           else (
             print_string (build_line a str) ;
             print_newline() ;
@@ -74,3 +76,63 @@ let triangle n str =
 
 triangle 5 "*" ;;
 triangle 6 "+" ;;
+
+
+(* 1.2.1 pyramid function prints a pyramid *)
+
+let pyramid n (str1, str2) =
+  let x = n
+  in let rec print_line n =
+       if n = 1 then (
+         print_string (build_line (x * 2) str2) ;
+         print_newline()
+       )
+       else (
+         let n1 = n - 1 and n2 = x - (n - 1)
+         in (
+             print_string (
+                 (build_line n1 str1) ^
+                   (build_line n2 str2) ^
+                     (build_line n2 str2) ^
+                       (build_line n1 str1)
+               ) ;
+             print_newline() ;
+             print_line (n - 1)
+           )
+       )
+     in print_line n
+;;
+
+pyramid 5 (".", "*") ;;
+pyramid 6 ("-", "|") ;;
+
+
+(* 1.2.2 cross function prints a cross *)
+
+let cross n (str1, str2) =
+  let x = n
+  in let rec print_line n =
+       let line_part_one = build_line (n - 1) str1
+       in if n = x then (
+            print_string (line_part_one ^ str2 ^ line_part_one) ;
+            print_newline()
+          )
+       else (
+            let line = (
+                line_part_one ^ str2 ^
+                  build_line ((2 * x) - 1 - (2 * n)) str1 ^
+                    str2 ^ line_part_one
+              )
+            in (
+                print_string line ;
+                print_newline() ;
+                print_line (n + 1) ;
+                print_string line ;
+                print_newline()
+              )
+          )
+     in print_line 1
+;;
+
+cross 5 (".", "&") ;;
+cross 6 (" ", "o") ;;
