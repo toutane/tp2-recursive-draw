7b6f3951df80ab19b66b4d92909b13ad11dd4b54
toutane <toutane.leger@gmail.com>
2021-10-12 13:12:40 +0200

    add bonuses functions.

--- fractals.ml
+++ fractals.ml
@@ -6,15 +6,13 @@ Random.init 1 ;;
 (* Load Graphics module on OSX with ocaml 4.0.2*)
 #load "graphics.cma"
 
-(* Load Greaphics module on Linux with ocaml 4.12 *)
+(* Load Graphics module on Linux with ocaml 4.12 *)
 #use "topfind" ;;
 #require "graphics" ;;
-
-(* Open Graphics library *)
 open Graphics ;;
 
 (* Show the graphics window *)
-open_graph " 800x600" ;;
+open_graph " 300x300" ;;
 
 (* draw_line function draws a line from point (x, y) to point (z, t) *)
 
@@ -34,7 +32,7 @@ let mountain n p1 p2 =
     if c = 1 then draw_line p1 p2
     else (
       let (x, y) = p1 and (z, t) = p2
-      in let h = (y + t) / 2 + (Random.int (10 * c ))
+      in let h = (y + t) / 2 + Random.int(abs(z - x)/5 + 20)
          in let m = ((x + z) / 2, h)
             in (
                 draw_mountain (c - 1) p1  m ;
@@ -44,7 +42,7 @@ let mountain n p1 p2 =
   in draw_mountain n p1 p2
 ;;
 
-mountain 10 (100, 200) (700, 200) ;;
+mountain 10 (0, 100) (400, 100) ;;
 
 
 (* 2.1.2 dragon function draws a dragon *)
@@ -124,3 +122,75 @@ let sierpinski_triangle n (x1, y1) (x2, y2) (x3, y3) =
 ;;
 
 sierpinski_triangle 5 (100, 50) (500, 50) (300, 346) ;;
+
+
+(* 2.3 Bonuses *)
+
+(* 2.3.1 Circles function draws  *)
+
+let circles (x, y) r r_limit =
+  let rec rec_circles (x, y) r =
+    let demi_r = r / 2
+    in if demi_r < r_limit then draw_circle x y r
+       else (
+         draw_circle x y r ;
+         rec_circles ((x + demi_r), y) demi_r ;
+         rec_circles ((x - demi_r), y) demi_r
+       )
+  in  rec_circles (x, y) r
+;;
+
+circles (100, 100) 100 5 ;;
+
+(* 2.3.1 Fleche function draws a fleche of radius r *)
+
+let fleche (ox, oy) rad n direction =
+  clear_graph() ;
+  set_color black ;
+  let rec repeat_draw (x, y) rad direction =
+    if (rad / 2) < n then (
+      fill_circle x y rad
+    )
+    else (
+      fill_circle x y rad ;
+      let new_rad = rad / 2
+      and coords_left = ((x - rand) - new_rad, y)
+      and coords_right = ((x + rad) + new_rad, y)
+      and coords_up = (x, ((y + rad) + new_rad))
+      and coords_down = (x, ((y - rad) - new_rad))
+      in match direction with
+           "up" -> (
+           repeat_draw coords_left new_rad "left" ;
+           repeat_draw coords_right new_rad "right";
+           repeat_draw coords_up new_rad "up"
+         )
+         | "left" -> (
+           repeat_draw coords_left new_rad "left";
+           repeat_draw coords_up new_rad "up" ;
+           repeat_draw coords_down new_rad "down"
+         )
+         | "right" -> (
+           repeat_draw coords_right new_rad "right" ;
+           repeat_draw coords_up new_rad "up" ;
+           repeat_draw coords_down new_rad "down"
+         )
+         | _ -> (
+           repeat_draw coords_left new_rad "left" ;
+           repeat_draw coords_right new_rad "right" ;
+           repeat_draw coords_down new_rad "down"
+             )
+         )
+  in repeat_draw (ox, oy) rad direction
+;;
+
+fleche (200, 200) 50 1 "up" ;;
+
+
+(* 2.3.3 koch_curve is a recursive function that draws a curve of von Koch of length d and order n. *)
+
+let koch_curve d =
+  clear_graph() ;
+  draw_line (100, 200) (100 + d, 200)
+;;
+
+koch_curve 100 ;;
